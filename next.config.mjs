edbd0f6b63f1eb1df73c7cf5ec8f508bb4b9616f
Baskara <>
2025-10-20 21:38:11 +0700

    Add R2 domain to Next.js image configuration for external images

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@ const nextConfig = {
   output: 'standalone',
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-2cb00cd498db403f87493524abf2de89.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   }
 };
 
